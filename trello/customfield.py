8a0c5b911111cb778207fd825f7237e1eab8c95c
Anton Sherkhonov <sherkhonov@gmail.com>
2018-03-14 20:36:19 +0000

    2nd fix attempt to adding option to custom field

--- trello/customfield.py
+++ trello/customfield.py
@@ -55,7 +55,7 @@ class CustomField(TrelloBase):
                  'value': {
                      'text': text }}
         obj = self.client.fetch_json(
-				'%s/customField' % self.id,
+				'/customField/%s' % self.id,
 				http_method='POST',
 				post_args=body)
 
